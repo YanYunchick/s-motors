94e90ad75140a857467875313062b28c454835e8
yan <yan.yunchick@gmail.com>
2023-05-02 18:25:21 +0300

    Task 1

--- force-app/main/default/lwc/sendInvoice/__tests__/sendInvoice.test.js
+++ force-app/main/default/lwc/sendInvoice/__tests__/sendInvoice.test.js
@@ -20,6 +20,6 @@ describe('c-send-invoice', () => {
 
         // Assert
         // const div = element.shadowRoot.querySelector('div');
-        expect(1).toBe(1);
+        expect(1).toBe(1); 
     });
 });
